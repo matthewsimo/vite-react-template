6dc955e4a1b64dcd4ff2a918ade09db7c192cdcb
Matthew Simo <matthew.a.simo@gmail.com>
2025-04-18 14:59:51 -0500

    add tanstack router to mix

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,19 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [
+    TanStackRouterVite({
+      target: "react",
+      autoCodeSplitting: true,
+      routesDirectory: "./src/react-app/routes",
+      generatedRouteTree: "./src/react-app/routeTree.gen.ts",
+    }),
+    react(),
+    cloudflare(),
+  ],
   build: {
     rollupOptions: {
       input: {
