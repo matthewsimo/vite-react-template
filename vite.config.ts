9408dd1587bf924041741274f7d842caae5de6d4
Matthew Simo <matthew.a.simo@gmail.com>
2025-04-18 13:01:43 -0500

    change not found handling to 404 page, add 404 entry point to vite config, other misc tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [react(), cloudflare()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        error: resolve(__dirname, "404.html"),
+      },
+    },
+  },
 });
